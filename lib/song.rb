cb85d5ab43e3d31fed3c47e511afcfdd41456c64
Amy Fogelman <abfnj1@gmail.com>
2019-07-21 22:24:58 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,24 +2,18 @@
 require "pry"
 
 class Song
-  
   attr_accessor :artist, :name
-  # attr_reader
-  
-  def initialize(name)
+
+   def initialize(name)
     @name = name
   end
 
-def self.new_by_filename(filename)
-  #song = filename.split(" - ")[1]
-  #song = Song.new(song_name)
-  #artist_object = Artist.find_or_create_by_name(filename)[0]
-  #song.artist = artist_object
-  #song.artist.add_song(self)
+   def artist_name
+    if artist
+      self.artist.name
+    else
+      nil
+    end
+  end
 
-  artist, title = filename.split(" - ")
-  self.new(title).tap(|song|
-  song.artist = Artist.find_or_create_by_name(artist)
-  song.artist.add_song(song)
-  )
-end
+ end 
